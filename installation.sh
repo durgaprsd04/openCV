46606f058c29f2debdad2d5a0f3a310fed8286a0
durgaprsd04 <durgaprsd04@gmail.com>
2014-02-08 22:54:32 +0530

    First Commit

---
+++ installation.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+echo Extract the opencv folder /Documents/opencvsource/
+apt-get install build-essential
+apt-get install gcc
+apt-get install g++
+apt-get install cmake 
+apt-get install pkg-config
+apt-get install python
+apt-get install python-numpy
+apt-get install ffmpeg
+apt-get install libavcodec-dev
+apt-get install libavformat-dev 
+apt-get install libswscale-dev
+cd ~/Documents/
+mkdir OpenCV-installation
+cd OpenCV-installation
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX= ~/Documents/opencvsource
+make
+make install
