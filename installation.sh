90f63f051d77c937c5115125f5609cbd41eb28e8
durgaprsd04 <durgaprsd04@gmail.com>
2014-02-10 09:21:04 +0530

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,7 +1,13 @@
 #!bin/bash
 echo Extract the opencv folder /Documents/opencvsource/
 sudo -s
-apt-get install -y build-essential && gcc && g++ && libgtk2.0-dev && python-dev && cmake && pkg-config && python && python-numpy 
+apt-get install -y build-essential  &&  && libgtk2.0-dev && python-dev && cmake && pkg-config && python && python-numpy 
+apt-get install -y gcc
+apt-get install -y g++
+apt-get install
+apt-get install
+apt-get install
+apt-get install
 && ffmpeg && libavcodec-dev && libavformat-dev && libswscale-dev
 cd ~/Documents/
 mkdir OpenCV-installation
